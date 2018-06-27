8fe0d3bb3fecdb5f550eb0f78edc0e68562d84fc
anna.hirai <tomato@gmail.com>
2018-06-27 00:34:48 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\Controller;
+
+use \App\Item;
+
 class ItemsController extends Controller
 {
      public function create()
@@ -35,5 +39,16 @@ class ItemsController extends Controller
             'keyword' => $keyword,
             'items' => $items,
         ]);
+    }
+    
+      public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
     }
 }
